bd879a29ed2cd0335bcddb0ecb3eb798a3ddc043
NourBahgat <nourbahgat2003@gmail.com>
2025-10-22 10:01:45 +0300

    logging server failing error in main

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,21 @@
 # backend/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
 
 
 app = FastAPI(title="Signal Viewer Backend")
 
 origins = [
-    "http://localhost:5173",  # Vite frontend
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
